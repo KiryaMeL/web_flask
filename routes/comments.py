32222c2bed138cd50b0071644d809a9c2297a1f8
Kirillsigma <caxap.mel@mail.ru>
2025-10-29 08:27:34 +0300

    final

--- routes/comments.py
+++ routes/comments.py
@@ -67,7 +67,7 @@ def delete_comment(comment_id):
 @jwt_required()
 def update_comment(comment_id):
     """
-    Редактирование комментария — доступно автору, admin и writer fs
+    Редактирование комментария — доступно автору, admin и writer
     """
     data = request.get_json() or {}
     new_text = (data.get('text') or '').strip()
